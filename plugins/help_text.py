e7d52cbae6710c1559c8ff8a3095c229234f02cd
madibag <62722024+madibag@users.noreply.github.com>
2020-04-12 16:34:02 +0300

    Add files via upload

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -82,14 +82,3 @@ async def upgrade(bot, update):
         disable_web_page_preview=True
     )
     
-    @pyrogram.Client.on_message(pyrogram.Filters.command(["rename"]))
-async def upgrade(bot, update):
-    # logger.info(update)
-    TRChatBase(update.from_user.id, update.text, "/rename")
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.REPLY_TO_DOC_FOR_RENAME_FILE,
-        parse_mode="html",
-        reply_to_message_id=update.message_id,
-        disable_web_page_preview=True
-    )
