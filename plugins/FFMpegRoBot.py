89114dd9c2229d4dcfb043a70dd7f69c44d58a8b
madibag <62722024+madibag@users.noreply.github.com>
2020-04-29 17:14:44 +0300

    Add files via upload

--- plugins/FFMpegRoBot.py
+++ plugins/FFMpegRoBot.py
@@ -25,14 +25,14 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 from helper_funcs.chat_base import TRChatBase
 from helper_funcs.display_progress import progress_for_pyrogram
-from helper_funcs.help_Nekmo_ffmpeg import take_screen_shot, cult_small_video
+from helper_funcs.help_Nekmo_ffmpeg import converlt_video
 
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 
 
-@pyrogram.Client.on_message(pyrogram.Filters.command(["ffmpegrobot"]))
-async def ffmpegrobot_ad(bot, update):
+@pyrogram.Client.on_message(pyrogram.Filters.command(["convert"]))
+async def convert(bot, update):
     if update.from_user.id not in Config.AUTH_USERS:
         await bot.delete_messages(
             chat_id=update.chat.id,
@@ -40,25 +40,7 @@ async def ffmpegrobot_ad(bot, update):
             revoke=True
         )
         return
-    TRChatBase(update.from_user.id, update.text, "ffmpegrobot")
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.FF_MPEG_RO_BOT_AD_VER_TISE_MENT,
-        disable_web_page_preview=True,
-        reply_to_message_id=update.message_id
-    )
-
-
-@pyrogram.Client.on_message(pyrogram.Filters.command(["trim"]))
-async def trim(bot, update):
-    if update.from_user.id not in Config.AUTH_USERS:
-        await bot.delete_messages(
-            chat_id=update.chat.id,
-            message_ids=update.message_id,
-            revoke=True
-        )
-        return
-    TRChatBase(update.from_user.id, update.text, "trim")
+    TRChatBase(update.from_user.id, update.text, "convert")
     saved_file_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".FFMpegRoBot.mkv"
     if os.path.exists(saved_file_path):
         a = await bot.send_message(
@@ -67,10 +49,10 @@ async def trim(bot, update):
             reply_to_message_id=update.message_id
         )
         commands = update.command
-        if len(commands) == 3:
+        if len(commands) == 1:
             # output should be video
-            cmd, start_time, end_time = commands
-            o = await cult_small_video(saved_file_path, Config.DOWNLOAD_LOCATION, start_time, end_time)
+            cmd = commands
+            o = await converlt_video(saved_file_path, Config.DOWNLOAD_LOCATION)
             logger.info(o)
             if o is not None:
                 await bot.edit_message_text(
@@ -89,52 +71,6 @@ async def trim(bot, update):
                     supports_streaming=True,
                     # reply_markup=reply_markup,
                     # thumb=thumb_image_path,
-                    reply_to_message_id=update.message_id,
-                    progress=progress_for_pyrogram,
-                    progress_args=(
-                        Translation.UPLOAD_START,
-                        a,
-                        c_time
-                    )
-                )
-                os.remove(o)
-                await bot.edit_message_text(
-                    chat_id=update.chat.id,
-                    text=Translation.AFTER_SUCCESSFUL_UPLOAD_MSG,
-                    disable_web_page_preview=True,
-                    message_id=a.message_id
-                )
-        elif len(commands) == 2:
-            # output should be screenshot
-            cmd, start_time = commands
-            o = await take_screen_shot(saved_file_path, Config.DOWNLOAD_LOCATION, start_time)
-            logger.info(o)
-            if o is not None:
-                await bot.edit_message_text(
-                    chat_id=update.chat.id,
-                    text=Translation.UPLOAD_START,
-                    message_id=a.message_id
-                )
-                c_time = time.time()
-                await bot.send_document(
-                    chat_id=update.chat.id,
-                    document=o,
-                    # thumb=thumb_image_path,
-                    # caption=description,
-                    # reply_markup=reply_markup,
-                    reply_to_message_id=update.message_id,
-                    progress=progress_for_pyrogram,
-                    progress_args=(
-                        Translation.UPLOAD_START,
-                        a,
-                        c_time
-                    )
-                )
-                c_time = time.time()
-                await bot.send_photo(
-                    chat_id=update.chat.id,
-                    photo=o,
-                    # caption=Translation.CUSTOM_CAPTION_UL_FILE,
                     reply_to_message_id=update.message_id,
                     progress=progress_for_pyrogram,
                     progress_args=(
